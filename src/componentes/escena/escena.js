667d8c2d96b4e8297bdfed99c88b01642c4f6f91
Juan Sebastián Soto Fernández <jsotof@localhost.localdomain>
2023-03-17 14:48:15 +0000

    Primer ejercicio

---
+++ src/componentes/escena/escena.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Escena() {
+    return (
+      <p>Nuestro héroe estaba flotando por el espacio sideral cuando a lo lejos divisó una nave espacial</p>
+    );
+  }
+
+export default Escena;
